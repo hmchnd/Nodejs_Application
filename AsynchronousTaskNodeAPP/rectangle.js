dd719e3e13a15d7a84db07bc9f656aeeba22da5d
Hem <shaktimagan@gmail.com>
2020-11-20 10:04:35 +0530

    Update rectangle.js

--- AsynchronousTaskNodeAPP/rectangle.js
+++ AsynchronousTaskNodeAPP/rectangle.js
@@ -1,1 +1,25 @@
 
+module.exports = (x,y,callback)=>{
+// error case
+  if(x<=0|| y<=0){
+    setTimeout(()=> callback(new Error("Area will be zero as length or breadth is zero")
+    , null),
+    2000);
+}  
+else{
+// success case
+ setTimeout(()=> callback( null, {
+perimeter :(x,y) => (2*(x+y)),
+area:(x,y)=> (x*y)
+
+ }),
+    2000);
+
+
+}
+
+}
+
+
+//exports.perimeter = (x,y)=> (2*(x+y));
+//exports.area = (x,y)=> (x*y);
